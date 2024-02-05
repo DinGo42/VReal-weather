c1c585bf0a8648d49b5c642c5da206822b78434f
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-02-05 11:33:45 +0200

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/VReal-weather",
   plugins: [react(), svgr(), tsconfigPaths()],
 });
