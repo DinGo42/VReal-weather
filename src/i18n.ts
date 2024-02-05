450c8b0bafab330af34c865c2be462ec956e7007
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-02-05 12:06:13 +0200

    done

--- src/i18n.ts
+++ src/i18n.ts
@@ -21,9 +21,8 @@ export const resources = {
 i18next.use(initReactI18next).init({
   ns: ["main"],
   defaultNS: "main",
-  fallbackLng: Translations.ENG,
+  fallbackLng: languageStorage.get() || Translations.ENG,
   resources,
-  lng: languageStorage.get() || Translations.ENG,
   fallbackNS: "main",
   interpolation: {
     escapeValue: false,
