da7b1f78e0ecf467684d882c78da258297646392
Ivan <91385595+DinGo42@users.noreply.github.com>
2024-02-05 11:46:53 +0200

    Updates

---
+++ icons/index.ts
@@ -0,0 +1,6 @@
+import SunIcon from "./sun.svg?react";
+import CloseIcon from "./close.svg?react";
+import PlanetIcon from "./planet.svg?react";
+import ArrowIcon from "./arrow.svg?react";
+
+export { SunIcon, CloseIcon, PlanetIcon, ArrowIcon };
